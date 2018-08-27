30cfb6d5cfdf7d0d36d6ab22906ce3d231093950
kharoshka <42418763+kharoshka@users.noreply.github.com>
2018-08-27 21:38:42 +0300

    Created Email Service

---
+++ EmailService/Managers/MailManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace EmailService
+{
+    public abstract class MailManager
+    {
+       public string GetAdminEmail() { return "admin@touristcenter.by"; }
+       public string GetCustomerEmail() { return "costomer@touristcenter.by"; }
+
+        private SmtpClient _smtp;
+        public SmtpClient Smtp
+        {
+            get
+            {
+                return _smtp ?? new SmtpClient
+                {
+                    Host = "pod51014.outlook.com",
+                    Port = 587,
+                    Credentials = new NetworkCredential("21241112@dut4life.ac.za", "Dut930611"),
+                    EnableSsl = true
+                };
+            }
+
+            private set
+            {
+                _smtp = value;
+            }
+
+        }
+
+       public abstract MailMessage CreateMailMessage();
+        
+    }
+}
