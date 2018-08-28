4283e64bb5eb98a04eeba159671c1981ee3aade6
kharoshka <42418763+kharoshka@users.noreply.github.com>
2018-08-28 22:05:14 +0300

    created EmailService assembly

---
+++ EmailService.Interfaces/IMailHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailService.Interfaces
+{
+    public interface IMailHelper
+    {
+        string Sender { get; set; }
+        string Recipient { get; set; }
+        string RecipientCC { get; set; }
+        string Subject { get; set; }
+        string Body { get; set; }
+        string AttachmentFile { get; set; }
+
+        void Send();
+
+    }
+}
