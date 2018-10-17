496fb8a7b10b674afe23002a4d267a7ef6af4f7c
Andrei Velichko <andr.velich@gmail.com>
2018-10-18 01:15:09 +0300

    footer & header layout

--- TouristCenter/src/app/Site/Footer/footer.component.ts
+++ TouristCenter/src/app/Site/Footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { OrderComponent } from '../Order/order.component';
 
 @Component({
     
@@ -9,4 +11,13 @@ import { Component } from "@angular/core";
 })
 export class FooterComponent {
 
+    constructor(
+        public dialog: MatDialog)
+    {
+        
+    }
+
+    public openOrderPopup() {
+        let dialogRef = this.dialog.open(OrderComponent);
+    }
 }
